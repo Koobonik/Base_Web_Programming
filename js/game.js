52bc7850c14aa4edf45863ad25a5f4c12295049a
Koobonik <kbi960130@naver.com>
2018-06-05 20:02:18 +0900

    평범한 계란 추가

--- js/game.js
+++ js/game.js
@@ -1,3 +1,16 @@
+/**
+ * 구현해야 하는 기능들
+ * 게임시작시
+ * 남은 수 처리하는 함수
+ * 실패할시 숫자 올려주는 함수
+ * 남은시간 계산 & 출력해주는 함수
+ * 게임이 진행될 -> 정답을 찾으세요 -> 실패시 "실패" 라고 출력
+ * 계란 랜덤 배치
+ * 웃는 계란 + 우는 계란 + 화내는 계란
+ * 랜덤으로 웃, 우, 화 계란을 찾으라고 지정해주는 함수
+ * 
+ */
+
 let Game = new object();// 
 Game.Left_chance = 3; // 남은 기회
 Game.Fail_number = 0; // 실패수
@@ -14,5 +27,7 @@ let Engry_egg = new Image(); // 화내는 계란
 Engry_egg.src = "img/engry_egg.jpg";
 
 function Game_start(){ // 게임 시작
-
+    
 }
+
+function 
