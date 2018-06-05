9419ba74b281e1beeb5221618d13c70befd7d009
Koobonik <kbi960130@naver.com>
2018-06-05 23:36:10 +0900

    틀마련

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-/**
+/*
  * 구현해야 하는 기능들
  * 게임시작시
  * 남은 수 처리하는 함수
@@ -10,12 +10,13 @@
  * 랜덤으로 웃, 우, 화 계란을 찾으라고 지정해주는 함수
  * 
  */
-
 let Game = new object();// 
 Game.Left_chance = 3; // 남은 기회
 Game.Fail_number = 0; // 실패수
 Game.Left_time = 15; // 남은시간
 
+let Egg = new Image();
+Egg.src = "img/egg.jpg";
 
 let Laugh_egg = new Image(); // 웃는 계란
 Lauth_egg.src = "img/laugh_egg.jpg";
@@ -26,8 +27,26 @@ Cry_egg.src = "img/cry_egg.jpg";
 let Engry_egg = new Image(); // 화내는 계란
 Engry_egg.src = "img/engry_egg.jpg";
 
-function Game_start(){ // 게임 시작
+function Game_start(){ // 게임 시작 전체적인 프로그램 흐름 제어 역할
+    Reset_variable(); // 변수 초기화
     
 }
 
-function 
+function Left_num(){ // 남은 수 처리 
+    Game.Left_chance = Game.Left_chance - 1;
+    return Game.Left_chance;
+}
+
+function Time(){
+
+}
+
+function Reset_variable(){ //게임 시작시 변수 초기화
+    Game.Left_chance = 3; // 남은 기회
+    Game.Fail_number = 0; // 실패수
+    Game.Left_time = 15; // 남은시간
+}
+
+function eggClick(){ // 클릭시 이벤트
+
+}
