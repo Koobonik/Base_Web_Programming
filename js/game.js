7a35582a3155ac141bb48a91c8fe5b5892410823
Koobonik <kbi960130@naver.com>
2018-06-05 19:44:49 +0900

    중간 커밋

--- js/game.js
+++ js/game.js
@@ -1,1 +1,18 @@
-var left_chance = 3; // 남은 기회
+let Game = new object();// 
+Game.Left_chance = 3; // 남은 기회
+Game.Fail_number = 0; // 실패수
+Game.Left_time = 15; // 남은시간
+
+
+let Laugh_egg = new Image(); // 웃는 계란
+Lauth_egg.src = "img/laugh_egg.jpg";
+
+let Cry_egg = new Image(); // 우는 계란
+Cry_egg.src = "img/cry_egg.jpg";
+
+let Engry_egg = new Image(); // 화내는 계란
+Engry_egg.src = "img/engry_egg.jpg";
+
+function Game_start(){ // 게임 시작
+
+}
