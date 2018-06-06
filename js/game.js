683cb0dd9ea4885bc700fd3a2acae1962255270d
Koobonik <kbi960130@naver.com>
2018-06-06 21:57:56 +0900

    타이머 구현

--- js/game.js
+++ js/game.js
@@ -28,22 +28,37 @@ function Game_start(){ // 게임 시작 전체적인 프로그램 흐름 제어 
 
 }
 
-function Left_num(){ // 남은 수 처리 
-    Game.Left_chance = Game.Left_chance - 1;
-    return Game.Left_chance;
-}
-
-function Time(){ // 남은 시간 계산
-    
-}
-
 function Reset_variable(){ //게임 시작시 변수 초기화
     alert("리셋 시작");
     let last_num = 6; // 찾아야 하는 계란 수
     let fail_num = 0; // 실패수는 당연히 0
     let last_time = 15; // 초기에 15초 줄 예정
-    alert(fail_num);
+    let game_status = true; // 게임 진행 유무 true or false
+    Time(last_time); // 시간 초기화
+    alert("리셋 시작");
 }
+
+function Time(last_time){ // 남은 시간 계산
+    let game_stop = setInterval(Out_put_time, 1000); // 1초마다 실행
+    function Out_put_time(){ // 남은 시간 계속해서 출력
+        last_time--;
+        if(last_time <= 0){ // 남은시간 0초이면 동작
+            document.getElementById("Left_time_box").innerHTML="<h5>남은 시간 : 0 초 </h5>";
+            clearInterval(game_stop); // 반복문 종료
+        }
+        else{ // 나머지는 1초마다 계속해서 함수 실행
+            document.getElementById("Left_time_box").innerHTML="<h5>남은 시간 : " + last_time+ " 초 </h5>";
+        }
+    }
+    
+}
+function Last_num(){ // 남은 수 처리 
+    Left_chance = Left_chance - 1;
+    return Left_chance ;
+}
+
+
+
 
 function eggClick(egg){ // 클릭시 이벤트
     alert("출력조차 안됨");
